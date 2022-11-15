146b8727bb35d1d58b14673670d4627ce2ce01fd
Janneth Guaman <janneth.guaman@unl.edu.ec>
2022-11-14 22:17:08 -0500

    cloud 1

--- SistemaVivento/settings.py
+++ SistemaVivento/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zucxt0+pq7h-yz_@sbqbl=^9o5a^ui3^s(p#qb$k8*1+%1#2n@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
